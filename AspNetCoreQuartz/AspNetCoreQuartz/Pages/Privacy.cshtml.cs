9408db504806d8f791a6c9681d074a9dadba3f07
damienbod <damien_bod@hotmail.com>
2021-11-07 17:25:19 +0100

    init App

---
+++ AspNetCoreQuartz/AspNetCoreQuartz/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetCoreQuartz.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
